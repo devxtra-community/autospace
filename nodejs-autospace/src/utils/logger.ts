e14f68c2d96f0bd7f5ac1d67a4eca3601b7db9cd
Shrijit Vivek <shrijitv3@gmail.com>
2025-12-28 17:50:39 +0530

    added logger utility

---
+++ nodejs-autospace/src/utils/logger.ts
@@ -0,0 +1,14 @@
+export const logError = (error: unknown, context?: string) => {
+  if (error instanceof Error) {
+    console.error(
+      `[ERROR]${context ? ` [${context}]` : ''}`,
+      error.message,
+    );
+    console.error(error.stack);
+  } else {
+    console.error(
+      `[ERROR]${context ? ` [${context}]` : ''}`,
+      error,
+    );
+  }
+};
