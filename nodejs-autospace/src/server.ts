4dd1b24999b4ae42a6d8d283a5b31e8d465a06ab
Shrijit Vivek <shrijitv3@gmail.com>
2025-12-28 17:00:47 +0530

    .env added in backend

--- nodejs-autospace/src/server.ts
+++ nodejs-autospace/src/server.ts
@@ -1,6 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import app from "./app";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ;
 
 app.listen(PORT, () => {
   console.log(`Backend running on port ${PORT}`);
