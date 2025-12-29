32825e89062551d8b5a1f821abd912a203f871c1
rashid <rashiee973@gmail.com>
2025-12-29 14:31:58 +0530

    configered the axios and baseurl with env

---
+++ nextjs-autospace/src/lib/api.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ;
+
+if(!BASE_URL){
+    throw new Error('base url in env not defined')
+}
+
+export const apiClient = axios.create({
+    baseURL : BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
+
+// TODO (Milestone 1):
+// - Add auth token interceptor
+// - Add response error handling
