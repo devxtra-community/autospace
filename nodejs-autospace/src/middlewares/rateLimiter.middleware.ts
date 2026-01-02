81fbff7340b11ad3a33a1fc58f9fedb737c48c4d
rashid <rashiee973@gmail.com>
2026-01-02 10:48:29 +0530

    chore(auth): add changes in rate limiter in authentication

--- nodejs-autospace/src/middlewares/rateLimiter.middleware.ts
+++ nodejs-autospace/src/middlewares/rateLimiter.middleware.ts
@@ -1,22 +1,22 @@
-import rateLimit from "express-rate-limit";
-import { Response, Request } from "express";
-import { sendAuthError } from "../utils/error";
-import { AuthErrorCode } from "../constants/auth.error";
+import rateLimit from 'express-rate-limit';
+import { Response, Request } from 'express';
+import { sendAuthError } from '../utils/error';
+import { AuthErrorCode } from '../constants/auth.error';
 
 export const authRateLimiter = rateLimit({
-    windowMs : 15 * 60 * 1000 ,
-    max : 5,
-    standardHeaders: true,
-    legacyHeaders: false,
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  standardHeaders: true,
+  legacyHeaders: false,
 
-    handler : (_req: Request, res: Response) => {
-       return sendAuthError(
-        res,
-        AuthErrorCode.RATE_LIMITED,
-        "Too many authentication requests , please try again later.",
-        429
-       );
-    },
+  handler: (_req: Request, res: Response) => {
+    return sendAuthError(
+      res,
+      AuthErrorCode.RATE_LIMITED,
+      'Too many authentication requests , please try again later.',
+      429,
+    );
+  },
 });
 
 // add limiting asper project moves like password reset limit //
