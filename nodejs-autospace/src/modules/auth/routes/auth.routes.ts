61dd19a8795aa7d38849097b08f2dab3db622bcd
rashid <rashiee973@gmail.com>
2026-01-03 16:13:20 +0530

    feat(auth): add protected route using JWT middleware

--- nodejs-autospace/src/modules/auth/routes/auth.routes.ts
+++ nodejs-autospace/src/modules/auth/routes/auth.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { register } from "../controllers/auth.controller";
+import { protectedRoute } from "../services/auth.service";
+import { authMiddleware } from "../../../middlewares/auth.middleware";
 
 const router = Router();
 
 router.post("/register", register);
+router.get("/protected", authMiddleware, protectedRoute);
 
 export default router;
