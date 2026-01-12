7e581ccd410cd8155afa988d8249e2a41a07dbe3
rashid <rashiee973@gmail.com>
2026-01-12 11:12:10 +0530

    feat(resource): add resource service

---
+++ nodejs-autospace/api-gateway/src/middleware/rateLimiter.middleware.ts
@@ -0,0 +1,64 @@
+import rateLimit from "express-rate-limit";
+import { Response, Request } from "express";
+import { sendAuthError } from "../utils/error";
+import { AuthErrorCode } from "../utils/error";
+import axios from "axios";
+import { servicesConfig } from "../config/service.config";
+
+export interface ServiceHealth {
+  name: string;
+  status: "up" | "down";
+  responseTime?: number;
+  url: string;
+}
+
+export const checkHealth = async (
+  serviceName: string,
+  serviceUrl: string,
+): Promise<ServiceHealth> => {
+  const startTime = Date.now();
+
+  try {
+    await axios.get(`${serviceUrl}/health`, { timeout: 5000 });
+    const responceTime = Date.now() - startTime;
+
+    return {
+      name: serviceName,
+      status: "up",
+      responseTime: responceTime,
+      url: serviceUrl,
+    };
+  } catch (error) {
+    return {
+      name: serviceName,
+      status: "down",
+      url: serviceUrl,
+    };
+  }
+};
+
+export const checkAllServices = async (): Promise<ServiceHealth[]> => {
+  return Promise.all(
+    Object.entries(servicesConfig).map(([serviceName, service]) =>
+      checkHealth(serviceName, service.url),
+    ),
+  );
+};
+
+export const authRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  standardHeaders: true,
+  legacyHeaders: false,
+
+  handler: (_req: Request, res: Response) => {
+    return sendAuthError(
+      res,
+      AuthErrorCode.RATE_LIMITED,
+      "Too many authentication requests , please try again later.",
+      429,
+    );
+  },
+});
+
+// add limiting asper project moves like password reset limit //
---
+++ nodejs-autospace/resource-service/src/modules/company/entities/company.entity.ts
@@ -0,0 +1,52 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Index,
+} from "typeorm";
+
+export enum CompanyStatus {
+  PENDING = "pending",
+  APPROVED = "approved",
+  REJECTED = "rejected",
+}
+@Entity("companies")
+export class Company {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @Index()
+  @Column("uuid")
+  ownerUserId!: string;
+
+  @Column({ type: "varchar", length: 150 })
+  companyName!: string;
+
+  @Index({ unique: true })
+  @Column({ type: "varchar", length: 100 })
+  businessRegistrationNumber!: string;
+
+  @Column({ type: "varchar", length: 200 })
+  businessLocation!: string;
+
+  @Column({ type: "varchar", length: 150 })
+  contactEmail!: string;
+
+  @Column({ type: "varchar", length: 20 })
+  contactPhone!: string;
+
+  @Column({
+    type: "enum",
+    enum: CompanyStatus,
+    default: CompanyStatus.PENDING,
+  })
+  status!: CompanyStatus;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
