49077b4d250bf152e903ae0c6a90254471d53466
rashid <rashiee973@gmail.com>
2026-01-12 17:46:37 +0530

    chore: format api-gateway

--- nodejs-autospace/api-gateway/src/index.ts
+++ nodejs-autospace/api-gateway/src/index.ts
@@ -1,4 +1,13 @@
-import "dotenv/config";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path:
+    process.env.NODE_ENV === "production"
+      ? ".env.prod"
+      : process.env.NODE_ENV === "stage"
+        ? ".env.stage"
+        : ".env",
+});
 
 import express, { urlencoded } from "express";
 import cors from "cors";
